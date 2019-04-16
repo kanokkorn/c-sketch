df8810ba5be0d84530162d614bb7e4f0798cccaf
kanokkorn <kanokorn@outlook.jp>
2019-04-16 22:08:15 +0700

    define set of pointer

--- dynamic_array.c
+++ dynamic_array.c
@@ -3,14 +3,13 @@
 
 int main() {
     int i, j;
-    /* TODO: define the 2D pointer variable here */
-    int nrows = 2;
-    int ncols = 2;
-    /* TODO: complete the following line to allocate memory for holding three rows */
-    int **pnumbers = (int **) malloc(nrows * sizeof(int *));
+    int **pnumbers;
 
-    /* TODO: allocate memory for storing the individual elements in a row */
+    pnumbers = (int **) malloc(3 * sizeof(int *));
+
     pnumbers[0] = (int *) malloc(1 * sizeof(int));
+    pnumbers[1] = (int *) malloc(2 * sizeof(int));
+    pnumbers[2] = (int *) malloc(3 * sizeof(int));
 
     pnumbers[0][0] = 1;
     pnumbers[1][0] = 1;
@@ -27,10 +26,10 @@ int main() {
     }
 
     for (i = 0; i < 3; i++) {
-        /* TODO: free memory allocated for each row */
+       free(pnumbers[i]);
     }
 
-    /* TODO: free the top-level pointer */
+    free(pnumbers);
 
   return 0;
 }
