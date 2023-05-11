052535d8819ff6554003b7f0d8fc03471002f2b8
kanokkorn <kanokorn@outlook.jp>
2023-05-11 22:55:05 +0700

    unix domain socket example from bard

---
+++ unix_socket.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+int main() {
+  // Create a socket.
+  int sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+  if (sockfd < 0) {
+    perror("socket");
+    exit(1);
+  }
+
+  // Bind the socket to a path.
+  struct sockaddr_un addr;
+  memset(&addr, 0, sizeof(addr));
+  addr.sun_family = AF_UNIX;
+  strncpy(addr.sun_path, "/tmp/my_socket", sizeof(addr.sun_path) - 1);
+  if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+    perror("bind");
+    exit(1);
+  }
+
+  // Listen for connections.
+  if (listen(sockfd, 5) < 0) {
+    perror("listen");
+    exit(1);
+  }
+
+  // Accept connections and print the messages sent by the clients.
+  while (1) {
+    int clientfd = accept(sockfd, NULL, NULL);
+    if (clientfd < 0) {
+      perror("accept");
+      exit(1);
+    }
+
+    char buffer[1024];
+    ssize_t bytes_read = read(clientfd, buffer, sizeof(buffer));
+    if (bytes_read < 0) {
+      perror("read");
+      exit(1);
+    }
+
+    printf("Received message from client: %s\n", buffer);
+
+    close(clientfd);
+  }
+
+  return 0;
+}
+
